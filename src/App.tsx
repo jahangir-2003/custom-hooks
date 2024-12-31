3b098de1d6b4b86c2f0770769042b9b17bfa111a
Jahangir Alam <jahangiralamnke786@gmail.com>
2024-12-31 19:06:47 +0530

    added context

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Button from './component/Button.tsx'
 import CustomHooks from './component/CustomHooks.tsx'
 import useFetchData from './component/MyHooks.tsx'
 import Rating from './component/Rating.tsx'
+import { ThemeContext } from './Context/ThemeContext.tsx'
+import Hook from './component/Hook.tsx'
 
 const App: React.FC = () => {
   const [count, setCount] = CustomHooks()
+  const { theme, ChangeTheme } = useContext(ThemeContext)
   const [show, setShow] = useState<string>("")
   const [data] = useFetchData('https://fakestoreapi.com/products')
   const [todo, setTodos] = useState<{ text: string, id: number, completed: boolean }[]>([]);
   const [newTodo, setNewTodo] = useState<string>("");
-
+  const [open, setOpen] = useState<boolean>(false)
+  console.log(theme)
   const customHooks = () => {
     return (
       <div className="flex items-center justify-center my-5 flex-col">
-        <h2 className="text-2xl capitalize font-serif my-4 underline underline-offset-4">Fetch and Render Data</h2>
+        <h2 className="text-2xl capitalize font-serif my-4 underline underline-offset-4">Fetch and Render Data using custom hooks</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-between w-full md:w-[95%] lg-[85%]">
           {data?.map((item) => (
             <div key={item.id} className="w-[300px] border-2 shadow-md rounded-md mx-auto">
@@ -48,11 +52,17 @@ const App: React.FC = () => {
   }
 
   const todos = () => {
+
+    const handleChange = () => { }
+    const addTodos = () => { }
+    const handleUpdate = () => { }
+    const handledelete = () => { }
+
     return (
-      <>
-        <div className="w-[90%] flex items-center mx-auto flex-row justify-between my-5">
+      <div className='w-[350px] h-screen border-2 relative border-purple-800 p-5 rounded-lg'>
+        <div className="flex items-center mx-auto flex-row justify-between">
           <h2 className="text-2xl font-bold">Today</h2>
-          <button className="bg-blue-300 px-3 py-2 rounded-lg">add</button>
+          <Button title='Add' onClick={() => setOpen(!open)} />
         </div>
 
         <div className="w-[90%] flex-col gap-10">
@@ -66,7 +76,6 @@ const App: React.FC = () => {
                     <input
                       type="checkbox"
                       checked={todo.completed}
-
                     />
                     <h2 className={todo.completed ? 'line-through' : ''}>{todo.text}</h2>
                     <button className="text-red-500">Delete</button>
@@ -77,13 +86,25 @@ const App: React.FC = () => {
             )
           }
         </div>
-      </>
+
+        <div className={`${open ? 'modal-open' : 'hidden'} absolute bg-slate-200  h-full w-full top-0 right-0 rounded-lg`}>
+          <div className="h-[300px] w-full bg-red-300 flex flex-col p-5 justify-center gap-6 rounded-lg">
+            <h2>Add to list</h2>
+            <input placeholder="enter todos " className="bg-transparent outline-none border-2 p-2 rounded-lg border-blue-800" />
+            <div className="flex flex-row justify-between ">
+              <Button className='h-10' title='cancel' onClick={() => setOpen(!open)} />
+              <Button className='h-10' title='done' onClick={() => { }} />
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 
   const renderContent = () => {
     if (show === "todos") return todos();
     if (show === "custom-hook") return customHooks();
+    if (show === "hooks") return <Hook />;
     return counter();
   }
 
@@ -91,8 +112,9 @@ const App: React.FC = () => {
     <>
       <div className="flex flex-row gap-5 items-center justify-center my-4">
         <button onClick={() => setShow("")} className="text-2xl capitalize underline underline-offset-4">Counter</button>
-        <button onClick={() => setShow("custom-hook")} className="text-2xl capitalize underline underline-offset-4">Custom Hooks</button>
+        <button onClick={() => setShow("custom-hook")} className="text-2xl capitalize underline underline-offset-4">fetch data</button>
         <button onClick={() => setShow("todos")} className="text-2xl capitalize underline underline-offset-4">Todos</button>
+        <button onClick={() => setShow("hooks")} className="text-2xl capitalize underline underline-offset-4">Hooks</button>
       </div>
 
       <div className="flex flex-col items-center">
--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ title, onClick, className }: { title: string, onClick: React.events, className?: string }) => {
     return (
-        <button onClick={onClick} className={`${className}  p-3 text-white bg-blue-900 hover:bg-blue-950 hover:text-green-200 hover:shadow-2xl duration-700    font-bold rounded-md`}>{title}</button>
+        <button onClick={onClick} className={`${className}  p-3 text-white bg-blue-900 hover:bg-blue-950 hover:text-green-200 hover:shadow-2xl duration-700 flex items-center justify-center   font-bold rounded-md`}>{title}</button>
     )
 }
 
